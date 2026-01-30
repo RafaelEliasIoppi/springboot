24426895d7b607a4b086082bb321e99c800e59d9
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2026-01-30 10:51:19 +0000

    rafaelk

--- src/main/java/br/com/rafaelelias/todolist/user/UserController.java
+++ src/main/java/br/com/rafaelelias/todolist/user/UserController.java
@@ -1,8 +1,18 @@
 package br.com.rafaelelias.todolist.user;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController    
+
+@RestController  
+@RequestMapping("/users")  
 public class UserController {
+
+
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel);
+
+    }
 
 }
